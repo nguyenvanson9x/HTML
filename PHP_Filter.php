bc6778d72bed1ec3c68b7f32a0879d80b7883827
NguyenVanSon <nguyenvanson_t59@hus.edu.vn>
2017-06-16 18:02:02 +0700

    php_filter

--- PHP_Filter.php
+++ PHP_Filter.php
@@ -1,5 +1,21 @@
 <?php
 $str = "<h1>Hello World</h1>";
 $newStr = filter_var($str, FILTER_SANITIZE_STRING);
-echo $newStr;
+echo $newStr . "<br>";
+
+$int = 0;
+
+if ( filter_var($int, FILTER_VALIDATE_INT) === 0 || !filter_var($int, FILTER_VALIDATE_INT) === false) {
+    echo("Integer is valid");
+} else {
+    echo("Integer is not valid");
+}
+echo "<br>";
+
+$ip = "127.0.0.1";
+if (!filter_var($ip, FILTER_VALIDATE_IP) === false) {
+  echo ("$ip is a valid IP address");
+} else {
+  echo ("$ip is not a valid IP address");
+}
 ?>
